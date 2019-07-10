f396fdfffd8258457bebedbc2ac5740c40f30f6a
Juan Antonio Mayoral <juan.mayoral@idiso.com>
2019-07-10 13:18:36 +0200

    Updates

---
+++ precache-manifest.00c8dc167441afeff6cfd088b8dbc608.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ae8b5302ce3e581757e3",
+    "url": "/portfolio/static/css/main.5d835c09.chunk.css"
+  },
+  {
+    "revision": "ae8b5302ce3e581757e3",
+    "url": "/portfolio/static/js/main.ae8b5302.chunk.js"
+  },
+  {
+    "revision": "1e60e41b319ce9753b2c",
+    "url": "/portfolio/static/js/1.1e60e41b.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "531ee7d109308baf9c4f368d5b575f3a",
+    "url": "/portfolio/index.html"
+  }
+];
